f0619535ccd6ae813f2b6847630f757a5bb92160
Namgyeon <119279127+Namgyeon@users.noreply.github.com>
2024-02-13 16:49:37 +0900

    delete구현, 수행한 일을 클릭하면 체크하는 기능 추가, 할 일 텍스트 입력후 입력값 초기화

--- main.js
+++ main.js
@@ -12,15 +12,29 @@ document.querySelector('#push').onclick = function(){
         ${document.querySelector('#newTask input').value}
       </span>
       <button class="delete">
-        <i class="fa-solid fa-delete-left"></i>
+        <i class="fa-solid fa-trash"></i>
       </button>
     </div>
     `;
+
+    // delete버튼을 누르면 할 일 리스트를 삭제하는 함수
+    const current_tasks = document.querySelectorAll('.delete');
+    for(let i=0; i<current_tasks.length; i++){
+      current_tasks[i].onclick = function(){
+        this.parentNode.remove();
+      }
+    }
+    
+    //완료한 일을 클릭했을때 줄이 간것을 표현.
+    var tasks = document.querySelectorAll('#tasks');
+    for(let i=0; i<tasks.length; i++){
+      tasks[i].onclick = function(){
+        this.classList.toggle('completed');
+      }
+    }
+
+    document.querySelector('#newTask input').value = '';
   }   
 }
 
-// delete버튼을 누르면 할 일 리스트를 삭제하는 함수
-document.querySelectorAll('.delete').onclick = function(){
-  
-}
 
